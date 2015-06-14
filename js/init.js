f6acee780f83786fdd6d8167fb21113cb29b70b9
Joe Bruzek <jbruzek@vt.edu>
2015-06-14 18:34:56 -0500

    initial commit

---
+++ js/init.js
@@ -0,0 +1,48 @@
+(function($) {
+    $(function() {
+
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+function toggleDark() {
+    var elements = document.getElementsByClassName('dark');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.classList.toggle("teal");
+        element.classList.toggle("darken-2");
+    }
+    elements = document.getElementsByClassName('dark1');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.classList.toggle("blue-grey");
+        element.classList.toggle("darken-4");
+    }
+
+    elements = document.getElementsByClassName('dark2');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.classList.toggle("blue-grey");
+        element.classList.toggle("darken-2");
+    }
+
+    elements = document.getElementsByClassName('darktext');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        if (element.style.color == "white") {
+            element.style.color = "black";
+        } else {
+            element.style.color = "white";
+        }
+    }
+
+    elements = document.getElementById("dark-toggle-item");
+    console.log(elements);
+    console.log(elements.innerHTML);
+    if (elements.innerHTML.substring(0, 4) == "Dark") {
+        elements.innerHTML = "Light Theme";
+    } else {
+        elements.innerHTML = "Dark Theme";
+    }
+}
